dab375762e6e651777801078289919c13347ce79
Alexander Boon <hi@alexanderboon.me>
2020-02-18 00:28:24 -0500

    Changed mouseover color to random/rainbow

--- script.js
+++ script.js
@@ -31,11 +31,20 @@ function createGrid(){
 function addMouseoverListen(){
     pieceSelector.forEach((piece) => {
         piece.addEventListener('mouseover', (e) => {
-            piece.style.backgroundColor = "black"; 
+            piece.style.backgroundColor = getRandomColor();
         });
     });
 };
-  
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    };
+    return color;
+};
+
 function buttonFunctionality(){
 
     document.querySelector('.clearButton');
