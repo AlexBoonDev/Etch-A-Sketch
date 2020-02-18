bd737e1ea4f0afa2b13a8e00735aa1cd7866da13
Alexander Boon <hi@alexanderboon.me>
2020-02-17 23:18:19 -0500

    added funcitonality for changing grid size

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
+// VARIABLES //
+
+let gridSize = 16;
+let pieceSize = 608/gridSize;
+let currentAttribute = "height: " + pieceSize + "px; width: " + pieceSize + "px; background-color: white;";
+const container = document.querySelector('#gridContainer');
+
+
+// FUNCTIONS //
 
 function createGrid(){
-    for (i = 0; i < 256; i++){
-        const container = document.querySelector('#gridContainer');
+
+    pieceSize = (608/gridSize);
+    currentAttribute = "height: " + pieceSize + "px; width: " + pieceSize + "px; background-color: white;";
+
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    };
+
+    for (i = 0; i < (gridSize**2); i++){
         const gridPiece = document.createElement('div');
         container.appendChild(gridPiece);
         gridPiece.classList.add('piece');
+        gridPiece.setAttribute('style', currentAttribute);    
     };
+
 };
 
 createGrid();
 
-const x = document.querySelectorAll('.piece');   
+// MOUSEOVER //
+
+let x = document.querySelectorAll('.piece');   
+
 x.forEach((piece) => {
     piece.addEventListener('mouseover', (e) => {
-        piece.setAttribute('style', 'background-color: black');    
+        piece.style.backgroundColor = "black"; 
     });
-  });
+});
 
-
+  // BUTTONS //
+  
 document.querySelector('.clearButton');
   clearButton.addEventListener('click', (e) => {
     x.forEach((piece) => {
-            piece.setAttribute('style', 'background-color: white');   
-            });
+        piece.setAttribute('style', currentAttribute); 
+    });
+});
+
+document.querySelector('.sizeButton');
+  sizeButton.addEventListener('click', (e) => {
+    gridSize = prompt("Choose the height/width of the grid");
+    createGrid();
 });
